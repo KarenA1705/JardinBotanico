51b850cd7fd47635f5dc27b3d8535887c8ebd66b
Fabian <fabianrincon1407@gmail.com>
2022-11-02 14:37:43 -0500

    validar email

--- controllers/entidades_CO.php
+++ controllers/entidades_CO.php
@@ -32,6 +32,13 @@ class entidades_CO
 
       exit(json_encode($arreglo_respuesta));
     }
+    if (!(filter_var($correo, FILTER_VALIDATE_EMAIL))) {
+      $arreglo_respuesta = [
+        "estado" => "ERROR",
+        "mensaje" => "por favor ingrese un correo valido"
+      ];
+      exit(json_encode($arreglo_respuesta));
+    }
     if (strlen($nit) > 20) {
       $arreglo_respuesta = [
         "estado" => "ERROR",
